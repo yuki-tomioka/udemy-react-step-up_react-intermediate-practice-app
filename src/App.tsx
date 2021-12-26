2ae8f0fb9d9cfb71951d1c4c77e446b319792d0c
Yuki Tomioka <yuki.tomioka@zozo.com>
2021-12-26 14:11:47 +0900

    Globalなスタイルの設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Button, ChakraProvider } from "@chakra-ui/react";
-import "./styles.css";
+import theme from "./theme/theme";
 
 export default function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button colorScheme="teal">ボタン</Button>
+      <p>aaaaa</p>
     </ChakraProvider>
   );
 }
