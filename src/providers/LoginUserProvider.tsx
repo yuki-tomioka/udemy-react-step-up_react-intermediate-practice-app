8fec07e9cca4b6a1d9dacfafb5753225e3986b68
Yuki Tomioka <yuki.tomioka.1016@gmail.com>
2022-02-13 18:22:17 +0900

    管理者ユーザーを想定してフラグをcontextに保持してみる

--- src/providers/LoginUserProvider.tsx
+++ src/providers/LoginUserProvider.tsx
@@ -8,9 +8,11 @@ import {
 
 import { User } from "../types/api/user";
 
+type LoginUser = User & { isAdmin: boolean };
+
 export type LoginUserContextType = {
-  loginUser: User | null;
-  setLoginUser: Dispatch<SetStateAction<User | null>>;
+  loginUser: LoginUser | null;
+  setLoginUser: Dispatch<SetStateAction<LoginUser | null>>;
 };
 
 export const LoginUserContext = createContext<LoginUserContextType>(
@@ -19,7 +21,7 @@ export const LoginUserContext = createContext<LoginUserContextType>(
 
 export const LoginUserProvider = (props: { children: ReactNode }) => {
   const { children } = props;
-  const [loginUser, setLoginUser] = useState<User | null>(null);
+  const [loginUser, setLoginUser] = useState<LoginUser | null>(null);
   return (
     <LoginUserContext.Provider value={{ loginUser, setLoginUser }}>
       {children}
